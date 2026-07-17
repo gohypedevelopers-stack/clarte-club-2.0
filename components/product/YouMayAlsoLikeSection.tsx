de0932dd77447d983d8d58f3553c6b4d8bc6ccaf
aniketthakur-404 <anikett331@gmail.com>
2026-07-17 14:38:11 +0530

    feat: add YouMayAlsoLikeSection component to product detail page

--- components/product/YouMayAlsoLikeSection.tsx
+++ components/product/YouMayAlsoLikeSection.tsx
@@ -13,7 +13,7 @@ export function YouMayAlsoLikeSection() {
           You May Also Like
         </h2>
 
-        <div className="mt-6 grid grid-cols-2 gap-4 xl:grid-cols-4">
+        <div className="mt-6 grid grid-cols-2 gap-4 lg:grid-cols-4">
           {trendingProducts.map((product) => (
             <ProductCardView key={product.id} product={product} />
           ))}
