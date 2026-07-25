e998780557ced5893094a013567309f46b42e2c6
aniketthakur-404 <anikett331@gmail.com>
2026-07-25 14:38:48 +0530

    feat: implement core landing page components including Hero, collection data, and launch offer countdown UI

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,13 @@ export default function Home() {
   return (
     <main className="flex-1 -mt-[var(--header-stack-height)] bg-black">
       <Hero />
-      <LaunchOfferBar />
       <TrendingSection />
       {/* <BrandPhilosophy /> */}
       <MotionBannerSection />
       <DenimCarousel />
       <EditsCarousel />
       <ComingSoonMarquee />
-      <DenimEditorialSection />
+      {/* <DenimEditorialSection /> */}
       <LookbookCarousel />
     </main>
   )
